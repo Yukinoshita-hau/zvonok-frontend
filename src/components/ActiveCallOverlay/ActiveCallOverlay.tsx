f4bf7fe899e31b3874ca48ec29c4aebfcec846de
RASHID_THE_FUCKER <Rashid16062006mor@yandex.ru>
2026-03-04 09:30:41 +0500

    допилин визуал камер в звонке

--- src/components/ActiveCallOverlay/ActiveCallOverlay.tsx
+++ src/components/ActiveCallOverlay/ActiveCallOverlay.tsx
@@ -1,26 +1,21 @@
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import styles from "./ActiveCallOverlay.module.css";
-import type { AppDispatch, RootState } from "../../store/store";
-import { callActions } from "../../store/slices/call.clice";
+import type {  RootState } from "../../store/store";
 import type { ActiveCallOverlayProps } from "./ActiveCallOverlay.props";
 import { LiveKitRoom } from "@livekit/components-react";
 import { CallUi } from "../CallUi/CallUi";
 import { useState } from "react";
 
 export function ActiveCallOverlay({ currentRoomId }: ActiveCallOverlayProps) {
-	const [callHeight, setCallHeight] = useState(50); // это в vh типа 50vh
+	const [callHeight, setCallHeight] = useState(55); // это в vh типа 50vh
 
 	const call = useSelector((s: RootState) => s.call);
-	const dispatch = useDispatch<AppDispatch>();
 
 	if ((call.chatRoomId !== currentRoomId) || (call.status !== "connecting" &&
 		call.status !== "in_call")) return null;
 
 	if (!call.serverUrl || !call.participantToken) return null;
 
-	const handleEnd = () => {
-		dispatch(callActions.endCall())
-	}
 
 	const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
 		const startY = e.clientY;
@@ -29,7 +24,7 @@ export function ActiveCallOverlay({ currentRoomId }: ActiveCallOverlayProps) {
 		const onMove = (moveEvent: MouseEvent) => {
 			const delta = moveEvent.clientY - startY;
 			const vhDelta = (delta / window.innerHeight) * 100;
-			const next = Math.min(65, Math.max(40, startHeight + vhDelta));
+			const next = Math.min(65, Math.max(55, startHeight + vhDelta));
 			console.log("onMove")
 			setCallHeight(next);
 		};
@@ -58,10 +53,6 @@ export function ActiveCallOverlay({ currentRoomId }: ActiveCallOverlayProps) {
 				</div>
 
 				<div className={styles["resize-handle"]} onMouseDown={handleMouseDown} />
-
-				<div className={styles["controls"]}>
-					<button className={styles["end-button"]} onClick={handleEnd}>Завершить</button>
-				</div>
 			</div>
 		</div>
 	)
--- src/components/CallUi/CallUi.tsx
+++ src/components/CallUi/CallUi.tsx
@@ -1,30 +1,63 @@
 import { TrackToggle, useTracks, VideoTrack } from "@livekit/components-react";
 import styles from "./CallUi.module.css";
 import { Track } from "livekit-client";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../../store/store";
+import { callActions } from "../../store/slices/call.clice";
 
 export function CallUi() {
 	const tracks = useTracks([
-		Track.Source.Camera,
+		{ source: Track.Source.Camera, withPlaceholder: true }
 	]);
+	const dispatch = useDispatch<AppDispatch>();
 
+	const onLeave = () => {
+		dispatch(callActions.endCall())
+	}
 	return (
 		<div className={styles["call-root"]}>
 			<div className={styles["participants-grid"]}>
 				{tracks.map((trackRef, index) => {
-					const { participant, publication, source } = trackRef;
+					const { participant, publication } = trackRef;
+
+					const hasVideo = "publication" in trackRef && !!publication?.trackSid;
 					return (
-						<div key={publication?.trackSid ?? index} className={styles["tile"]}>
-							<VideoTrack trackRef={trackRef} />
+						<div key={publication?.trackSid ?? participant.identity ?? index} className={styles["tile"]}>
+							<div className={styles["media"]}>
+								{hasVideo ? (
+									<VideoTrack trackRef={trackRef} />
+								) : (
+									<div className={styles["placeholder"]}>
+										<div className={styles["avatar"]}>
+											{participant.identity?.[0]?.toUpperCase()}
+										</div>
+									</div>
+								)}
+							</div>
 							<div className={styles["name"]}>{participant.identity}</div>
-							<div className={styles["source"]}>{source}</div>
 						</div>
 					)
 				})}
 			</div>
 			<div className={styles["controls-bar"]}>
-				<TrackToggle source={Track.Source.Camera}>
-					Камера
-				</TrackToggle>
+				<TrackToggle
+					source={Track.Source.Microphone}
+					className={styles["controll-button"]}
+				/>
+				<TrackToggle
+					source={Track.Source.Camera}
+					className={styles["controll-button"]}
+				/>
+				<TrackToggle
+					source={Track.Source.ScreenShare}
+					className={styles["controll-button"]}
+				/>
+				<button
+					className={styles["leave-button"]}
+					onClick={onLeave}
+				>
+					<img src="../../../public/leave-call-icon.svg" />
+				</button>
 			</div>
 		</div>
 	)
